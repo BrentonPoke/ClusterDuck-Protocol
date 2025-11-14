d4391eae1456a618cb53f65162c62cc2ef69fc15
BrentonPoke <brentonpoke@outlook.com>
2025-11-14 23:35:46 +0000

    Deploying to gh-pages from  @ 153d92fd1a29d8d63c63216811cef1598881e2c7 🚀

--- search/all_c.js
+++ search/all_c.js
@@ -3,6 +3,5 @@ var searchData=
   ['neighbor_0',['neighbor',['../class_neighbor.html',1,'Neighbor'],['../class_neighbor.html#a4f0e2a8d4037dd998a690607dcd19a54',1,'Neighbor::Neighbor()']]],
   ['neighbor_2eh_1',['Neighbor.h',['../_neighbor_8h.html',1,'']]],
   ['net_5fjoin_5fdelay_2',['NET_JOIN_DELAY',['../_duck_8h.html#aa5b0baecb8e40a2b8cfe92a5bf1c2300',1,'Duck.h']]],
-  ['networkstate_3',['NetworkState',['../_duck_router_8h.html#a350d04c55f1ef121ca2c7396cae452a0',1,'DuckRouter.h']]],
-  ['now_4',['now',['../classduckutils_1_1_timer.html#a59230a76aaebaa2892dc79314fc9d8d1',1,'duckutils::Timer']]]
+  ['networkstate_3',['NetworkState',['../_duck_router_8h.html#a350d04c55f1ef121ca2c7396cae452a0',1,'DuckRouter.h']]]
 ];
--- search/functions_b.js
+++ search/functions_b.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['neighbor_0',['Neighbor',['../class_neighbor.html#a4f0e2a8d4037dd998a690607dcd19a54',1,'Neighbor']]],
-  ['now_1',['now',['../classduckutils_1_1_timer.html#a59230a76aaebaa2892dc79314fc9d8d1',1,'duckutils::Timer']]]
+  ['neighbor_0',['Neighbor',['../class_neighbor.html#a4f0e2a8d4037dd998a690607dcd19a54',1,'Neighbor']]]
 ];
